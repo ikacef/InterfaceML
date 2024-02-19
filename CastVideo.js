d36d2d8f5aab16d08813d844de49bc4bf46ac693
ikacef <cefcurlz@gmail.com>
2024-02-19 15:59:30 -0500

    avanced

--- CastVideo.js
+++ CastVideo.js
@@ -1,53 +1,55 @@
-let currentSession;
-let currentMediaSession;
-let isPlaying = true;
-let currentVideoIndex = 0;
-let currentVideoUrl;
-let updateInterval;
-const seekSlider = document.getElementById('seekSlider');
-const currentTimeElement = document.getElementById('currentTime');
-const totalTimeElement = document.getElementById('totalTime');
-const defaultContentType = 'video/mp4';
-const applicationID = '3DDC41A0';
-const videoList = [
-    'https://transfertco.ca/video/DBillPrelude.mp4',
-    'https://transfertco.ca/video/DBillSpotted.mp4',
-    'https://transfertco.ca/video/usa23_7_02.mp4'
-    // Add more video URLs as needed
-];
+document.addEventListener('DOMContentLoaded', function() {
+    
 
-document.getElementById('connectButton').addEventListener('click', () => {
-    initializeApiOnly();
-});
+    let currentSession;
+    let currentMediaSession;
+    let isPlaying = true;
+    let currentVideoIndex = 0;
+    let currentVideoUrl;
+    let updateInterval;
+    const seekSlider = document.getElementById('seekSlider');
+    const currentTimeElement = document.getElementById('currentTime');
+    const totalTimeElement = document.getElementById('totalTime');
+    const defaultContentType = 'video/mp4';
+    const videoList = [
+        'https://transfertco.ca/video/DBillPrelude.mp4',
+        'https://transfertco.ca/video/DBillSpotted.mp4',
+        'https://transfertco.ca/video/usa23_7_02.mp4'
+        // Ajoutez plus d'URL de vidéos au besoin
+    ];
 
-document.getElementById('startBtn').addEventListener('click', () => {
-    if (currentSession) {
-        loadMedia(videoList[currentVideoIndex]);
-    } else {
-        alert('Connectez-vous sur chromecast en premier');
-    }
-});
+    document.getElementById('connectButton').addEventListener('click', () => {
+        initializeApiOnly();
+    });
 
-document.getElementById('nextBtn').addEventListener('click', () => {
-    if (currentSession) {
-        currentVideoIndex = (currentVideoIndex + 1) % videoList.length;
-        loadMedia(videoList[currentVideoIndex]);
-    } else {
-        alert('Connectez-vous sur chromecast en premier');
-    }
-});
-
-document.getElementById('playBtn').addEventListener('click', () => {
-    if (currentMediaSession) {
-        if (isPlaying) {
-            currentMediaSession.pause(null, onMediaCommandSuccess, onError);
+    document.getElementById('startBtn').addEventListener('click', () => {
+        if (currentSession) {
+            loadMedia(videoList[currentVideoIndex]);
         } else {
-            currentMediaSession.play(null, onMediaCommandSuccess, onError);
+            alert('Connectez-vous sur chromecast en premier');
         }
-        isPlaying = !isPlaying;
-    }
-});
+    });
 
+    document.getElementById('nextBtn').addEventListener('click', () => {
+        if (currentSession) {
+            currentVideoIndex = (currentVideoIndex + 1) % videoList.length;
+            loadMedia(videoList[currentVideoIndex]);
+        } else {
+            alert('Connectez-vous sur chromecast en premier');
+        }
+    });
+
+    document.getElementById('playBtn').addEventListener('click', () => {
+        if (currentMediaSession) {
+            if (isPlaying) {
+                currentMediaSession.pause(null, onMediaCommandSuccess, onError);
+            } else {
+                currentMediaSession.play(null, onMediaCommandSuccess, onError);
+            }
+            isPlaying = !isPlaying;
+        }
+    });  
+});
 
 function sessionListener(newSession) {
     currentSession = newSession;
@@ -102,7 +104,7 @@ function onMediaCommandSuccess() {
 
 function initializeApiOnly() {
     
-    const sessionRequest = new chrome.cast.SessionRequest(applicationID);
+    const sessionRequest = new chrome.cast.SessionRequest(chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID);
     const apiConfig = new chrome.cast.ApiConfig(sessionRequest, sessionListener, receiverListener);
 
     chrome.cast.initialize(apiConfig, onInitSuccess, onError);
@@ -125,4 +127,7 @@ function formatTime(timeInSeconds) {
     const minutes = Math.floor(timeInSeconds / 60);
     const seconds = Math.floor(timeInSeconds % 60);
     return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-}
+}  
+
+
+
